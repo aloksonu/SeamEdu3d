ac43ae90e0b63a6ba8c610514ecb602418b411da
Alok Dubey <alok.dubey@seamedu.com>
2025-02-05 14:40:42 +0530

    Model imported

--- Assets/FirstProject/Scripts/ModelManager.cs
+++ Assets/FirstProject/Scripts/ModelManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModelManager : MonoBehaviour
 {
+    [SerializeField] private Animator _Animator;
+    [SerializeField] private Button _buttonIdle;
+    [SerializeField] private Button _buttonRun;
+    [SerializeField] private Button _buttonJump;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _Animator.SetTrigger("Idle");
+        //_Animator.Play("Idle");
+        _buttonIdle.onClick.AddListener(PlayIdle);
+        _buttonRun.onClick.AddListener(PlayRun);
+        _buttonJump.onClick.AddListener(PlayJump);
+    }
+    private void OnDestroy()
+    {
+        _buttonIdle.onClick.RemoveAllListeners();
+        _buttonIdle.onClick.RemoveAllListeners();
+    }
+    private void PlayIdle()
+    {
+        _Animator.SetTrigger("Idle");
+        //_Animator.Play("Idle");
+    }
+    private void PlayJump()
+    {
+        //_Animator.SetTrigger("Jump");
+        //_Animator.Play("Jump");
+    }
+    private void PlayRun()
+    {
+        _Animator.SetTrigger("Run");
+        //_Animator.Play("Drunk Run Forward");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
